240ab552d48ba64708dcdb12dae3e10db3badb5f
sergiodueck <se_dueck@hotmail.com>
2020-12-21 23:52:58 -0300

    update final

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -138,19 +138,19 @@ function elevarAlCubo(num) {
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  return math.pow(num,exponent);
+  return Math.pow(num,exponent);
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  return math.round(num);
+  return Math.round(num);
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-return math.ceil (num);
+return Math.ceil (num);
 }
 
 function agregarSimboloExclamacion(str) {
